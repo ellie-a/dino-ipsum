b742e6c9580d3cebafacad9bd800625ae4b8c42f
Ellie Angell and Jim Padilla <ellie+jim@>
2017-01-04 10:18:06 -0800

    take dina and return array and hold letter entered

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,15 +1,29 @@
 var Dino = require('./../js/dino.js').dinoModule;
 
+
 $(document).ready(function() {
-  var getDinos      = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=3'),
+  var api_word = "";
+  var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1'),
     fillContainer = function(html) {
-      $('#ipsum').html(html);
+      $('#ipsum').text(html);
+      api_word = getDinos.responseJSON[0][0];
+      console.log(api_word);
+      console.log(getDinos.responseJSON[0][0]);
     },
     oops = function() {
       console.log('Where did all the dinosaurs go?');
     };
 
-getDinos.then(fillContainer, oops);
+    getDinos.then(fillContainer, oops);
 
+    var newDino = new Dino(api_word);
+
+    $('#submit').click(function(){
+      var guessed = $('#guessedLetter').val();
+      newDino['api_word'] = getDinos.responseJSON[0][0];
+      newDino['guessed'] = guessed;
+      console.log(newDino);
+      newDino.checker();
+    });
 
 });
--- js/dino.js
+++ js/dino.js
@@ -1,5 +1,18 @@
-function Dino(api_word) {
-  this.current_word = api_word;
+function Dino() {
+  this.api_word = "";
+  this.guessed = "";
 }
+
+Dino.prototype.checker = function(){
+  var wordArray = this.api_word.split('');
+  var letter = this.guessed;
+  console.log(wordArray);
+  console.log(letter);
+  while(wordArray.indexOf(letter) !== -1){
+    var current_index = wordArray.indexOf(letter);
+    wordArray.splice(current_index, 1);
+  }
+
+};
 
 exports.dinoModule = Dino;
